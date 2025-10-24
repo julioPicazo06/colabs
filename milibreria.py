5d6ac10b508c2e315b14d04a8102f36d00b05784
jpicazo <juliopicazo06@gmail.com>
2025-10-23 20:57:02 -0600

    feat(function): add list sum, average calculation, and custom library functions

---
+++ milibreria.py
@@ -0,0 +1,9 @@
+
+
+def saludar(nombre):
+    """imprime un saludo por pantalla"""
+    return f"Hola {nombre} , bienvenido a la libreria"
+
+def promedio(lista):
+    """Calcula el pormedio de una lsitsa de numeros"""
+    return sum(lista) / len(lista)
